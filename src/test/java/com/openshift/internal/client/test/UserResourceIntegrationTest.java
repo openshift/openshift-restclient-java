222bdc647653f0ec2fb1d385d9fc86cc03e04faf
André Dietisheim <adietish@redhat.com>
2012-08-06 16:29:49 +0200

    added user integration tests

--- src/test/java/com/openshift/internal/client/test/UserResourceIntegrationTest.java
+++ src/test/java/com/openshift/internal/client/test/UserResourceIntegrationTest.java
@@ -10,16 +10,24 @@
  ******************************************************************************/
 package com.openshift.internal.client.test;
 
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
+import java.util.List;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.openshift.client.IDomain;
 import com.openshift.client.IUser;
 import com.openshift.client.InvalidCredentialsOpenShiftException;
 import com.openshift.client.OpenShiftException;
+import com.openshift.client.utils.DomainTestUtils;
 import com.openshift.client.utils.OpenShiftTestConfiguration;
 import com.openshift.client.utils.TestConnectionFactory;
 
@@ -39,5 +47,59 @@ public class UserResourceIntegrationTest {
 	public void shouldThrowIfInvalidCredentials() throws Exception {
 		new TestConnectionFactory().getConnection(
 				new OpenShiftTestConfiguration().getClientId(), "bogus-password").getUser();	
+	}
+
+	@Test
+	public void shouldGetDefaultDomain() throws OpenShiftException {
+		// precondition
+		DomainTestUtils.getFirstDomainOrCreate(user);
+		// operation
+		IDomain domain = user.getDefaultDomain();
+		// verification
+		assertNotNull(domain);
+		assertNotNull(domain.getId());
+		assertTrue(domain.getId().length() > 0);
+		assertNotNull(domain.getSuffix());
+		assertTrue(domain.getSuffix().length() > 0);
+	}
+
+	@Test
+	public void shouldReturnThatHasDomain() throws OpenShiftException {
+		// precondition
+		DomainTestUtils.getFirstDomainOrCreate(user);
+		// operation
+		Boolean hasDomain = user.hasDomain();
+		// verification
+		assertTrue(hasDomain);
+	}
+
+	@Test
+	public void shouldReturnThatHasNoDomain() throws OpenShiftException {
+		// precondition
+		DomainTestUtils.silentlyDestroyAllDomains(user);
+		// operation
+		Boolean hasDomain = user.hasDomain();
+		// verification
+		assertFalse(hasDomain);
+	}
+
+	@Test
+	public void shouldReturnEmptyDomains() throws OpenShiftException {
+		// precondition
+		DomainTestUtils.silentlyDestroyAllDomains(user);
+		// operation
+		List<IDomain> domains = user.getDomains();
+		// verification
+		assertThat(domains).isEmpty();
+	}
+
+	@Test
+	public void shouldReturnDomains() throws OpenShiftException {
+		// precondition
+		DomainTestUtils.getFirstDomainOrCreate(user);
+		// operation
+		List<IDomain> domains = user.getDomains();
+		// verification
+		assertThat(domains).isNotEmpty();
 	}
 }
