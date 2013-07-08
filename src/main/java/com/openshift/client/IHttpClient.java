4ff2fe3075776eda4e0d3b0c8ad461b231c8e904
André Dietisheim <adietish@redhat.com>
2013-07-08 12:25:02 +0200

    [JBIDE-15146] corrected IHttpClient#STATUS_INTERNAL_SERVER_ERROR

--- src/main/java/com/openshift/client/IHttpClient.java
+++ src/main/java/com/openshift/client/IHttpClient.java
@@ -37,7 +37,7 @@ public interface IHttpClient {
 	public static final String AUTHORIZATION_BASIC = "Basic";
 
 	public static final int STATUS_OK = 200;
-	public static final int STATUS_INTERNAL_SERVER_ERROR = 200;
+	public static final int STATUS_INTERNAL_SERVER_ERROR = 500;
 	public static final int STATUS_BAD_REQUEST = 400;
 	public static final int STATUS_UNAUTHORIZED = 401;
 	public static final int STATUS_NOT_FOUND = 404;
