64e2f28de1fa62524e694a85ce1718e5045cd86c
André Dietisheim <adietish@redhat.com>
2013-09-09 15:45:42 +0200

    [JBIDE-15164] added swtichyard, cron embedded cartridges
    Addded name constants and LatestVersionOf accessors for cron and
    switchyard

--- src/main/java/com/openshift/client/cartridge/selector/LatestVersionOf.java
+++ src/main/java/com/openshift/client/cartridge/selector/LatestVersionOf.java
@@ -20,28 +20,16 @@ import com.openshift.client.cartridge.IStandaloneCartridge;
  */
 public class LatestVersionOf {
 
-	public static LatestEmbeddableCartridge mySQL() {
-		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_MYSQL);
-	}
-
-	public static LatestEmbeddableCartridge phpMyAdmin() {
-		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_PHPMYADMIN);
-	}
-
-	public static  LatestEmbeddableCartridge postgreSQL() {
-		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_POSTGRESQL);
-	}
-	
-	public static  LatestEmbeddableCartridge mongoDB() {
-		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_MONGODB);
-	}
-
-	public static  LatestEmbeddableCartridge rockMongo() {
-		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_ROCKMONGO);
-	}
-
 	public static  LatestEmbeddableCartridge mmsAgent() {
 		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_10GEN_MMS_AGENT);
+	}
+
+	public static  LatestEmbeddableCartridge cron() {
+		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_CRON);
+	}
+
+	public static  LatestEmbeddableCartridge haProxy() {
+		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_HAPROXY);
 	}
 
 	public static  LatestEmbeddableCartridge jenkinsClient() {
@@ -52,8 +40,28 @@ public class LatestVersionOf {
 		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_METRICS);
 	}
 
-	public static  LatestEmbeddableCartridge haProxy() {
-		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_HAPROXY);
+	public static  LatestEmbeddableCartridge mongoDB() {
+		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_MONGODB);
+	}
+
+	public static LatestEmbeddableCartridge phpMyAdmin() {
+		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_PHPMYADMIN);
+	}
+
+	public static  LatestEmbeddableCartridge postgreSQL() {
+		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_POSTGRESQL);
+	}
+
+	public static LatestEmbeddableCartridge mySQL() {
+		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_MYSQL);
+	}
+
+	public static  LatestEmbeddableCartridge rockMongo() {
+		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_ROCKMONGO);
+	}
+
+	public static LatestEmbeddableCartridge switchyard(){
+		return new LatestEmbeddableCartridge(IEmbeddableCartridge.NAME_SWITCHYARD);
 	}
 
 	public static LatestStandaloneCartridge jbossEws(){
