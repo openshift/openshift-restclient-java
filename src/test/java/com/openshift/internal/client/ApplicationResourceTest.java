3652abde005f9f75ef8a5a7a24f3c6004ca0f689
Corey <cdaley@redhat.com>
2014-03-26 08:08:07 -0400

    [OSJC-54] Adding tests for ApplicationSSHSession

--- src/test/java/com/openshift/internal/client/ApplicationResourceTest.java
+++ src/test/java/com/openshift/internal/client/ApplicationResourceTest.java
@@ -505,45 +505,6 @@ public class ApplicationResourceTest extends TestTimer {
 	}
 
 	@Test
-	public void shouldGetForwardablePorts() throws Throwable {
-		// pre-conditions
-		final IApplication app = domain.getApplicationByName("springeap6");
-		assertThat(app).isNotNull().isInstanceOf(ApplicationResource.class);
-		String[] rhcListPortsOutput = new String[] {
-				"haproxy -> 127.7.233.2:8080",
-				" haproxy -> 127.7.233.3:8080",
-				" java -> 127.7.233.1:3528",
-				" java -> 127.7.233.1:4447",
-				" java -> 127.7.233.1:5445",
-				" java -> 127.7.233.1:5455",
-				" java -> 127.7.233.1:8080",
-				" java -> 127.7.233.1:9990",
-				" java -> 127.7.233.1:9999",
-				" mysql -> 5190d701500446506a0000e4-foobarz.rhcloud.com:56756" };
-		ApplicationResource spy = Mockito.spy(((ApplicationResource) app));
-		JSch jsch = new JSch();
-		final IApplicationSSHSession ses = new ApplicationSSHSession(spy, jsch.getSession("mockuser", "mockhost", 22));
-		ApplicationSSHSession spyses = Mockito.spy(((ApplicationSSHSession) ses));
-		Mockito.doReturn(Arrays.asList(rhcListPortsOutput)).when(spyses)
-				.sshExecCmd(Mockito.anyString(), (ApplicationSSHSession.SshStreams) Mockito.any());
-
-		// operation
-		List<IApplicationPortForwarding> forwardablePorts = spyses.getForwardablePorts();
-
-		// verification
-		assertThat(forwardablePorts).isNotEmpty().hasSize(10);
-		assertThat(forwardablePorts)
-				.onProperty("name").containsExactly("haproxy", "haproxy", "java", "java", "java", "java", "java",
-						"java", "java", "mysql");
-		assertThat(forwardablePorts)
-				.onProperty("remoteAddress").containsExactly("127.7.233.2", "127.7.233.3", "127.7.233.1",
-						"127.7.233.1", "127.7.233.1", "127.7.233.1", "127.7.233.1", "127.7.233.1", "127.7.233.1",
-						"5190d701500446506a0000e4-foobarz.rhcloud.com");
-		assertThat(forwardablePorts)
-				.onProperty("remotePort").containsExactly(8080, 8080, 3528, 4447, 5445, 5455, 8080, 9990, 9999, 56756);
-	}
-
-	@Test
 	public void shouldCanGetEnvironmentVariables() throws Throwable {
 		// pre-conditions
 		mockDirector
