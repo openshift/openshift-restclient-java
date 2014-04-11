d272646a7c7124110560dfeab3050d55cd1565ee
Andre Dietisheim <adietish@redhat.com>
2014-04-11 15:51:39 +0200

    [OSJC-109] implemented IQuickstart#getAlternativesFor(ICartridge)

--- src/test/java/com/openshift/client/utils/QuickstartTestUtils.java
+++ src/test/java/com/openshift/client/utils/QuickstartTestUtils.java
@@ -19,6 +19,22 @@ import com.openshift.client.IQuickstart;
  */
 public class QuickstartTestUtils {
 
+	public static final String AEROGEAR_PUSH_0X = "AeroGear Push 0.X";
+	public static final String CACTI = "Cacti";
+	public static final String CAKEPHP = "CakePHP";
+	public static final String CAPEDWARF = "CapeDwarf";
+	public static final String CARTRIDGE_DEVELELOPMENT_KIT = "Cartridge Development Kit";
+	public static final String DJANGO = "Django";
+	public static final String DRUPAL_8 = "Drupal 8";
+	public static final String GO_LANGUAGE = "Go Language";
+	public static final String JBOSS_FUSE_61 = "JBoss Fuse 6.1";
+	public static final String LARAVEL_41 = "Laravel 4.1 Quickstart";
+	public static final String REVEALJS = "Reveal.js";
+	public static final String RUBY_ON_RAILS = "Ruby on Rails";
+	public static final String TEXTPRESS = "TextPress";
+	public static final String WILDFLY_8 = "WildFly 8";
+	public static final String WORDPRESS_3X = "WordPress 3.x";
+	
 	public static IQuickstart getByName(String name, List<IQuickstart> quickstarts) {
 		IQuickstart matchingQuickstart = null;
 		for (IQuickstart quickstart : quickstarts) {
