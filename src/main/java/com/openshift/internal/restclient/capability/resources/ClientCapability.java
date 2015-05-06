931732a11b5f5f095a61d2ce04210c3313d82fee
Jeff Cantrill <jcantril@redhat.com>
2015-05-06 12:26:48 -0400

    [OSJC-162] Add capability for a resource to expose its client

---
+++ src/main/java/com/openshift/internal/restclient/capability/resources/ClientCapability.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.openshift.internal.restclient.capability.resources;
+
+import com.openshift.restclient.IClient;
+import com.openshift.restclient.capability.resources.IClientCapability;
+
+/**
+ * Implementation to retrieve the client from a resource
+ * @author jeff.cantrill
+ */
+public class ClientCapability implements IClientCapability {
+	
+	private IClient client;
+	
+	public ClientCapability(IClient client) {
+		this.client = client;
+	}
+	
+	@Override
+	public boolean isSupported() {
+		return client != null;
+	}
+
+	@Override
+	public String getName() {
+		return getClass().getSimpleName();
+	}
+
+	@Override
+	public IClient getClient() {
+		return client;
+	}
+
+}
---
+++ src/main/java/com/openshift/restclient/capability/resources/IClientCapability.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.openshift.restclient.capability.resources;
+
+import com.openshift.restclient.IClient;
+import com.openshift.restclient.capability.ICapability;
+
+/**
+ * The capability to retrieve the client from a resource
+ * @author jeff.cantrill
+ *
+ */
+public interface IClientCapability extends ICapability {
+	
+	IClient getClient();
+}
