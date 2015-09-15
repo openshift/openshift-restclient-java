411668022c0de303bad3cfa943af8340beb22087
Jeff Cantrill <jcantril@redhat.com>
2015-09-15 11:49:09 -0400

    [JBIDE-20682] Fix webhook url secret case-sensitivity

--- src/test/java/com/openshift/internal/restclient/model/build/WebhookTriggerTest.java
+++ src/test/java/com/openshift/internal/restclient/model/build/WebhookTriggerTest.java
@@ -29,13 +29,13 @@ public class WebhookTriggerTest {
 	}
 	@Test
 	public void testGetWebhookUrlWhenResourceHasBaseURL() {
-		trigger = new WebhookTrigger(BuildTriggerType.GENERIC, "secret101","https://localhost:8443/oapi/v1/namespaces/test/buildconfigs/foo");
-		assertEquals("https://localhost:8443/oapi/v1/namespaces/test/buildconfigs/foo/webhooks/secret101/generic", trigger.getWebhookURL());
+		trigger = new WebhookTrigger(BuildTriggerType.GENERIC, "seCRet101","https://localhost:8443/oapi/v1/namespaces/test/buildconfigs/foo");
+		assertEquals("https://localhost:8443/oapi/v1/namespaces/test/buildconfigs/foo/webhooks/seCRet101/generic", trigger.getWebhookURL());
 	}
 	
 	@Test
 	public void testGetWebhookUrlWhenResourceDoesNotHaveBaseURL(){
-		trigger = new WebhookTrigger(BuildTriggerType.GENERIC, "secret101"," ");
+		trigger = new WebhookTrigger(BuildTriggerType.GENERIC, "seCRet101"," ");
 		assertEquals("",trigger.getWebhookURL());
 	}
 }
