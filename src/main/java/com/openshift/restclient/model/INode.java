a26ab8bafc5ecb0b4c5617a596dd711cc5e2e25c
Hyunsoo Kim <hyun.soo.kim1112@gmail.com>
2015-11-04 11:30:51 +0900

    add resource quota fuction

---
+++ src/main/java/com/openshift/restclient/model/INode.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
+package com.openshift.restclient.model;
+
+import java.util.List;
+
+/**
+ * @author Hyunsoo Kim
+ */
+public interface INode extends IResource {
+
+	/**
+	 * Retrieves resource of the given kind that are scoped to
+	 * this Node
+	 * @param kind
+	 * @return List<IResources>
+	 */
+	<T extends IResource> List<T> getResources(String kind);
+
+	String getDisplayName();
+	
+	void setDisplayName(String name);
+	
+	String getDescription();
+	
+	void setDescription(String value);
+	
+}
