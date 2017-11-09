8c43df4a285f0091d821e2879ec0bf8ab17f1ecd
Andre Dietisheim <adietish@redhat.com>
2017-11-09 13:54:32 +0100

    impl'd rudimentary support for readiness- and lifeness-probes

---
+++ src/main/java/com/openshift/restclient/model/probe/IProbe.java
@@ -0,0 +1,32 @@
+/******************************************************************************* 
+ * Copyright (c) 2017 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package com.openshift.restclient.model.probe;
+
+/**
+ * @author Andre Dietisheim
+ */
+public interface IProbe {
+
+	void setInitialDelaySeconds(int delay);
+	int getInitialDelaySeconds();
+
+	void setPeriodSeconds(int period);
+	int getPeriodSeconds();
+
+	void setSuccessThreshold(int threshold);
+	int getSuccessThreshold();
+
+	void setFailureThreshold(int failureThreshold);
+	int getFailureThreshold();
+
+	void setTimeoutSeconds(int timeout);
+	int getTimeoutSeconds();
+}
