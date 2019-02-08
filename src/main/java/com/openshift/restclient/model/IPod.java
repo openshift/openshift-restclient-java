e1f3fc9529e5ab190a980b70c5827f1f706c7401
Andre Dietisheim <adietish@redhat.com>
2019-02-08 14:30:30 +0100

    corrected and completed javadocs
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/main/java/com/openshift/restclient/model/IPod.java
+++ src/main/java/com/openshift/restclient/model/IPod.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * Copyright (c) 2015-2018 Red Hat, Inc. Distributed under license by Red Hat, Inc.
  * All rights reserved. This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -15,45 +15,64 @@ import java.util.Set;
 public interface IPod extends IResource {
 
     /**
-     * Gets the IP of the Pod
+     * Returns the IP of this pod.
      * 
+     * @return ip of this pod.
      */
     String getIP();
 
     /**
-     * Gets the name of the host on which the pod is running
+     * Returns the hostname of the host on which the pod is running.
+     * 
+     * @return the hostname of this pod.
      * 
      */
     String getHost();
 
     /**
-     * Gets the collection of image names for the pod containers
+     * Returns the names of the images that the containers of this pod are using.
      * 
+     * @return the image names for the containers of this pod.
      */
     Collection<String> getImages();
 
     /**
-     * Gets the status of the pod
+     * Returns the status of the pod. The pod status is derived from the status of
+     * all it's containers. The current implementation is limited to the status of
+     * the 1st container though. The string that's returned is built out of
+     * different properties of the container:
+     * <ul>
+     * <li>state.waiting.reason</li>
+     * <li>state.terminated.reason</li>,
+     * <li>state.terminated.signal</li>
+     * <li>state.terminated.exitCode</li>
+     * </ul>
      * 
+     * @return the status of this pod
      */
     String getStatus();
 
     /**
-     * Retrieve the set of ports that the containers are using
+     * Returns the ports that the containers of this pod are using.
+     * 
+     * @returns the ports of the containers for this pod.
      */
     Set<IPort> getContainerPorts();
 
     /**
-     * Add a container with the given name. This is useful if creating a pod
-     * directly without a resource controller
+     * Adds a container with the given name. This is useful if creating a pod
+     * directly without a resource controller. Returns the container for the given name.
      * 
+     * @param the name of the container
+     * 
+     * @return the container that was added.
      */
     IContainer addContainer(String name);
 
     /**
-     * Retrieve all the containers spec'd for the pod
+     * Returns all the containers for this pod.
      * 
-     * @return collection of containers
+     * @return the containers for this pod.
      */
     Collection<IContainer> getContainers();
 }
