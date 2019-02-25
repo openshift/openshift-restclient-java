95bbb06a7480194085c33f6ceadeccd8bbfe5b3f
Andre Dietisheim <adietish@redhat.com>
2019-02-25 19:57:39 +0100

    [365] ignoring ITs for deprecated authorization endpoints
    * Role
    * PolicyBinding
    * Policy
    
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/test/java/com/openshift/internal/restclient/authorization/AuthorizationKindsIntegrationTest.java
+++ src/test/java/com/openshift/internal/restclient/authorization/AuthorizationKindsIntegrationTest.java
@@ -14,6 +14,7 @@ import static org.fest.assertions.Assertions.assertThat;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.openshift.internal.restclient.IntegrationTestHelper;
@@ -31,17 +32,20 @@ public class AuthorizationKindsIntegrationTest {
         client = helper.createClientForBasicAuth();
     }
 
+    @Ignore("Role endpoint was deprecated in v1")
     @Test
     public void testListRolesAssumingClusterAdmin() {
         List<IRole> roles = client.list(ResourceKind.ROLE, IntegrationTestHelper.getDefaultNamespace());
         assertThat(roles).isNotEmpty();
     }
 
+    @Ignore("Policy endpoint was deprecated in v1")
     @Test
     public void testListPoliciesAssumingClusterAdmin() {
         client.list(ResourceKind.POLICY, IntegrationTestHelper.getDefaultNamespace());
     }
 
+    @Ignore("PolicyBinding endpoint was deprecated in v1")
     @Test
     public void testListPolicyBindingsAssumingClusterAdmin() {
         client.list(ResourceKind.POLICY_BINDING, IntegrationTestHelper.getDefaultNamespace());
