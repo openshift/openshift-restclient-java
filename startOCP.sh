5cff8632fdbd3b829dba05d7ac970a83c74a6fb4
Jeff MAURY <jmaury@redhat.com>
2019-10-05 10:58:53 +0200

    Fix download URL
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- startOCP.sh
+++ startOCP.sh
@@ -1,4 +1,4 @@
-curl https://mirror.openshift.com/pub/openshift-v3/$1/linux/oc.tar.gz --location --output oc.tar.gz
+curl https://mirror.openshift.com/pub/openshift-v3/clients/$1/linux/oc.tar.gz --location --output oc.tar.gz
 gunzip oc.tar.gz
 tar -xvf oc.tar
 ./oc cluster up
